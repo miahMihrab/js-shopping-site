e798fe2c58966b77ae1a5dbaa519bf66f716e397
Mihrab Miah <mihrabmiah13@gmail.com>
2019-09-13 21:40:02 +0600

    little design change

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
 const watchlist = document.querySelector(".watch-list");
 const productNames = document.querySelector('#productNames');
+//console.log(document.querySelector('.nav-bar').children[1])
+
 const shopProducts = ['watch', 'cloth']
 
+document.querySelector('.nav-bar').addEventListener('mouseover', e => {
+
+    console.log(Array.from(e.target.classList))
+    if (Array.from(e.target.classList).includes('bar')) {
+        console.log('true')
+        const drop_nav = document.querySelector('.dropdown_nav');
+        // if (Array.from(drop_nav).includes('hide')) {
+        //     drop_nav.classList.remove('hide');
+        //     drop_nav.classList.add('show');
+        // } else {
+        //     drop_nav.classList.remove('show');
+        //     drop_nav.classList.add('hide');
+        // }
+    }
+})
 
 let p = document.querySelector('.nav-bar').children[5].children[0];
 //console.log(abc)
